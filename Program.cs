ca13d4c3a9849daba3f53cc3208adb57ee7ac467
Cosmo Rodrigues <cosmo.rsilva@gmail.com>
2023-11-25 00:53:53 -0300

    Exercise 01 - Done

--- Program.cs
+++ Program.cs
@@ -1,2 +1,31 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+public class ChallangeSolcutions
+{
+  // Challnge 01
+  public static bool CompareTrue(bool ligado, bool quente)
+  {
+    bool podeBanhar;
+    if (ligado && quente)
+    {
+      podeBanhar = true;
+    }
+    else
+    {
+      podeBanhar = false;
+    }
+
+    return podeBanhar;
+  }
+}
+
+class Program
+{
+  static void Main()
+  {
+    bool resultado01 = ChallangeSolcutions.CompareTrue(true, true);
+    bool resultado02 = ChallangeSolcutions.CompareTrue(false, true);
+
+    Console.WriteLine(resultado01);
+    Console.WriteLine(resultado02);
+  }
+}
